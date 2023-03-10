3cc53646951980e5ab4c96f1fd8d7369dbce011b
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-10 12:03:44 +0100

    Add total prime number.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int m, n, count=0;
-        m=2;
-        n=15;
+        int m, n, count=0, totalPrime=0;
+        m=10;
+        n=30;
         for(int i= m; i<=n; i++){
             for(int j=2; j<i; j++){
                 if(i%j==0){
@@ -10,9 +10,11 @@ public class Main {
                     break;
                    }
                 }
-            if(count==0)
+            if(count==0){
                 System.out.println(i);
+                totalPrime++;}
             count=0;
         }
+        System.out.println("Total prime number = " +totalPrime);
     }
 }
